f1d8e72be3b34cf5fa26354e87b3e225c876db6d
Tom <tomtarpeydev@gmail.com>
2020-02-11 08:30:43 +0000

    day 2 skeleton

---
+++ day2.py
@@ -0,0 +1,34 @@
+# Passing by value Vs passing by Ref
+
+# define a doubling function that passes args by value
+def mult2(x):
+    pass
+
+# define a doubling function that passes args by reference
+def mult2_list(l):
+    pass
+
+
+# try out the functions
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#===========================================================
+
+# Return the "centered" average of an array of ints, which we'll say is the mean average of the values, 
+# except ignoring the largest and smallest values in the array. 
+
+# centered_average([1, 2, 3, 4, 100]) → 3
+# centered_average([1, 1, 5, 5, 10, 8, 7]) → 5
+# centered_average([-10, -4, -2, -4, -2, 0]) → -3
